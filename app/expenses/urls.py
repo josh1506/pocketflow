dced6b8df40553aecdc387b16987b1024f261145
josh1506 <jmjabor@gmail.com>
2025-03-31 23:55:39 +0800

    Add Expenses Chart & Improve Income UI

--- app/expenses/urls.py
+++ app/expenses/urls.py
@@ -1,13 +1,21 @@
 from django.urls import path
 
+from .ajax_views import load_expenses_chart_data
 from .views import ExpensesListView, ExpensesCreateView, ExpensesDetailView, ExpensesUpdateView, ExpensesDeleteView
 
 app_name = "expenses"
 
-urlpatterns = [
+ui_patterns = [
     path('', ExpensesListView.as_view(), name='list'),
     path('create/', ExpensesCreateView.as_view(), name='create'),
     path('details/<str:expense_id>/', ExpensesDetailView.as_view(), name='details'),
     path('update/<str:expense_id>/', ExpensesUpdateView.as_view(), name='update'),
     path('delete/<str:expense_id>/', ExpensesDeleteView.as_view(), name='delete'),
 ]
+
+
+api_patterns = [
+    path('chart-data/', load_expenses_chart_data, name='expenses_chart_data'),
+]
+
+urlpatterns = ui_patterns + api_patterns
