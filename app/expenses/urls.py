d7249b25992727908e9cbdea1b54dfedfb88041d
josh1506 <jmjabor@gmail.com>
2025-03-31 18:03:06 +0800

    Add Expenses Update Feature

--- app/expenses/urls.py
+++ app/expenses/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import ExpensesListView, ExpensesCreateView, ExpensesDetailView
+from .views import ExpensesListView, ExpensesCreateView, ExpensesDetailView, ExpensesUpdateView
 
 app_name = "expenses"
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', ExpensesListView.as_view(), name='list'),
     path('create/', ExpensesCreateView.as_view(), name='create'),
     path('details/<str:expense_id>/', ExpensesDetailView.as_view(), name='details'),
+    path('update/<str:expense_id>/', ExpensesUpdateView.as_view(), name='update'),
 ]
