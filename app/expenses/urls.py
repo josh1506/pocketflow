90d93b5181def580bfade7930b474a70390d8182
josh1506 <jmjabor@gmail.com>
2025-03-31 17:44:34 +0800

    Add Expenses Details Page

--- app/expenses/urls.py
+++ app/expenses/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import ExpensesListView, ExpensesCreateView
+from .views import ExpensesListView, ExpensesCreateView, ExpensesDetailView
 
 app_name = "expenses"
 
 urlpatterns = [
     path('', ExpensesListView.as_view(), name='list'),
     path('create/', ExpensesCreateView.as_view(), name='create'),
+    path('details/<str:expense_id>/', ExpensesDetailView.as_view(), name='details'),
 ]
