856c59e0a7117d3e508ba95386dcfcfb422c5c4e
josh1506 <jmjabor@gmail.com>
2025-04-13 01:21:37 +0800

    Implement landing page and signup improvements

--- config/urls.py
+++ config/urls.py
@@ -19,9 +19,11 @@ from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.views.generic import TemplateView
 from app.auths.views import SignUpView
 
 urlpatterns = [
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path('admin/', admin.site.urls),
     path("income/", include("app.income.urls"), name="income"),
     path("expenses/", include("app.expenses.urls"), name="expenses"),
